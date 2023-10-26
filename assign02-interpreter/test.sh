1a377f7a0ce95b9cda14c593aa737c6363bde14d
superboy-zjc <1826599908@qq.com>
2023-10-25 23:30:40 -0400

    assign03

--- assign02-interpreter/test.sh
+++
@@ -1,4 +0,0 @@
-export ASSIGN02_DIR=/home/ubuntu/interpreters/Compilers-and-Interpreters/assign02-interpreter
-cd /home/ubuntu/interpreters/fall2023-tests/assign02
-./run_all.rb
-./run_test.rb function02
