3460d92186104d7d77d4c4a5f8c009c4140e064f
superboy-zjc <1826599908@qq.com>
2023-12-01 23:18:11 -0500

    i..

--- test_low_level.sh
+++ test_low_level.sh
@@ -1,6 +1,6 @@
 export ASSIGN05_DIR=~/Compilers-and-Interpreters
 TEST_DIR=~/fall2023-tests/assign05
-cd $TEST_DIR && ./run_test.rb -o example$1
+cd $TEST_DIR && ./run_test.rb example$1
 #$TEST_DIR/build.rb $TEST_DIR/example$1
 #cat  ~/fall2023-tests/assign04/my_output/example$1.out
 #cat  ~/fall2023-tests/assign04/my_output_low/example$1.out
